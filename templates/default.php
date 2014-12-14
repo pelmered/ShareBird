f8ce143cf92be52ee9cf09b8f2a8ee51f225b13b
khromov <stanislav.khromov@gmail.com>
2014-12-15 00:38:57 +0100

    Remove default counts

--- templates/default.php
+++ templates/default.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $options = ShareBird()->get_options();
 $counts = ShareBird()->get_counts();
 ?>
@@ -9,7 +9,7 @@ $counts = ShareBird()->get_counts();
                 <li>
                     <a class="sharebutton facebook" data-basecount="0" data-sharetype="facebook" data-text="<?php echo ShareBird()->get_post_title('facebook'); ?>" title="<?php _e('Share this on Facebook', $plugin_slug); ?>" href="#">
                         <i class="icon-facebook"></i>
-                        <span class="count"><?php echo $counts['facebook']; ?></span>
+                        <span class="count"></span>
                     </a>
                 </li>
             <?php endif; ?>
@@ -18,7 +18,7 @@ $counts = ShareBird()->get_counts();
                 <li>
                     <a class="sharebutton twitter" data-basecount="0" data-sharetype="twitter" data-text="<?php echo ShareBird()->get_post_title('twitter'); ?>" data-via="<?php echo ShareBird()->get_author('twitter'); ?>" data-related="<?php the_author(); ?>" title="<?php _e('Share this on Twitter', $plugin_slug); ?>" href="#">
                         <i class="icon-twitter"></i>
-                        <span class="count">10</span>
+                        <span class="count"></span>
                     </a>
                 </li>
             <?php endif; ?>
@@ -27,7 +27,7 @@ $counts = ShareBird()->get_counts();
                 <li>
                     <a class="sharebutton linkedin" data-basecount="0" data-sharetype="linkedin" data-text="<?php echo ShareBird()->get_post_title('linkedin'); ?>" data-referer="<?php echo ShareBird()->get_author('linkedin'); ?>" data-related="<?php echo ShareBird()->get_author('linkedin'); ?>" title="<?php _e('Share this on LinkedIn', $plugin_slug); ?>" href="#">
                         <i class="icon-linkedin"></i>
-                        <span class="count">0</span>
+                        <span class="count"></span>
                     </a>
                 </li>
             <?php endif; ?>
@@ -36,7 +36,7 @@ $counts = ShareBird()->get_counts();
                 <li>
                     <a class="sharebutton googleplus" data-basecount="0" data-sharetype="googleplus" data-text="<?php echo ShareBird()->get_post_title('googleplus'); ?>" title="<?php _e('Share this on Google Plus', $plugin_slug); ?>" href="#">
                         <i class="icon-googleplus">
-                        </i> <span class="count">0</span>
+                        </i> <span class="count"></span>
                     </a>
                 </li>
             <?php endif; ?>
